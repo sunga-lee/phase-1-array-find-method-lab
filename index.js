2148708ae8ce4142ca6d1d54bbb99c6a1260a09a
sunga-lee <sungajunior1@gmail.com>
2023-03-24 18:09:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function superbowlWin(record){
+ let entry = record.find(function(element){
+    
+    return element.result === "W"
+
+ })
+ 
+ if(entry !== undefined)
+   return entry.year
+
+   return entry 
+ 
+}
+
+
+
+// / code your solution here
+// indexOf()
+// /find()
+// filter()
+
+// reduce()
+// map()
+// forEach()
